5ed7db96f24d41787a43832c6e67aecd5a2d0f3d
noeliarico <noeliarico@uniovi.es>
2020-06-29 10:33:24 +0200

    Add test parse_ranking

--- tests/testthat/test-parse_ranking.R
+++ tests/testthat/test-parse_ranking.R
@@ -9,3 +9,10 @@ test_that("Letters with ties", {
   expect_equal(as.numeric(parse_ranking("a ~ d ~ b ~ c")), c(1, 1, 1, 1))
   expect_equal(as.numeric(parse_ranking("b ≻ a ≻ d ~ c")), c(2, 1, 3, 3))
 })
+
+test_that("Letters and number with ties", {
+  expect_equal(as.numeric(parse_ranking("C2 ~ C3 ~ C4 ≻ C1 ~ C5")), c(2, 1, 1, 1, 2))
+  expect_equal(as.numeric(parse_ranking("C3 ∼ C5 ≻ C4 ≻ C1 ∼ C2")), c(3, 3, 1, 2, 1))
+  expect_equal(as.numeric(parse_ranking("C3 ∼ C5 ≻ C2 ≻ C1 ∼ C4")), c(3, 2, 1, 3, 1))
+})
+
