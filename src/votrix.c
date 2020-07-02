05033996b4362dbe425620c44066cd36c5c831e1
Noelia <noeliarico@uniovi.es>
2020-07-02 11:54:25 +0200

    Update votrix

--- src/votrix.c
+++ src/votrix.c
@@ -7,41 +7,10 @@ void votrix(int *profileOfRankings,
             int *ncandidates, 
             int *nrankings,
             int *votrix) {
-  
-  //  numberOfVoters       ranking
-  //  1              6 c ≻ b ≻ a ≻ d
-  //  2              5 a ≻ d ≻ b ≻ c
-  //  3              3 b ≻ a ≻ d ≻ c
-  
-  // votes
-  // [6, 5, 3]
-  
-  // profile of rankings
-  // [3, 2, 1, 4,   1, 3, 4, 2,   2, 1, 4, 3]
-  
-  // results
-  // [0, 0, 0, 0, number of times that a is better than a, b, c, d
-  //  0, 0, 0, 0, number of times that b is better than a, b, c, d
-  //  0, 0, 0, 0, number of times that c is better than a, b, c, d
-  //  0, 0, 0, 0] number of times that d is better than a, b, c, d
-  
-  // results after
-  // [0, 1, 2, 3, number of times that a is better than a, b, c, d
-  //  1, 0, 2, 2, number of times that b is better than a, b, c, d
-  //  1, 1, 0, 1, number of times that c is better than a, b, c, d
-  //  0, 1, 2, 0] number of times that d is better than a, b, c, d
-  
-  printf("\nEstoy dentro de votrix\n");
-  
-  
+
   int c1, c2, r, total;
   int nc = *ncandidates;
   int nr = *nrankings;
-  int totpos = nc * nr;
-  
-  printf("nc: %d \n", nc);
-  printf("nr: %d \n", nr);
-  printf("totpos: %d \n", totpos/2);
   
   for(c1 = 0; c1 < nc; c1++) { // for each of the candidates
     for(c2 = c1+1; c2 < nc; c2++) { // evaluate the rest of the candidates
@@ -51,15 +20,17 @@ void votrix(int *profileOfRankings,
         // the rankings are stored in a vector
         if(profileOfRankings[c1+(r*nc)] > profileOfRankings[c2+(r*nc)]) {
           total++;
+          printf("c%d [%d] is better than c%d [%d] in ranking %d, total = %d \n", c1+1, profileOfRankings[c1+(r*nc)], c2+1, profileOfRankings[c2+(r*nc)], r+1, total);
         }
         else if(profileOfRankings[c1+(r*nc)] == profileOfRankings[c2+(r*nc)]){
           total+=0.5;
+          printf("c%d [%d] is tied with c%d [%d] in ranking %d, total = %d \n", c1+1, profileOfRankings[c1+(r*nc)], c2+1, profileOfRankings[c2+(r*nc)], r+1, total);
         }
-        printf("Is %d > %d?\n", profileOfRankings[c1+(r*nc)], profileOfRankings[c2+(r*nc)]);
+        else {
+          printf("c%d [%d] is worst than c%d [%d] in ranking %d, total = %d \n", c1+1, profileOfRankings[c1+(r*nc)], c2+1, profileOfRankings[c2+(r*nc)], r+1, total);
+        }
+        // printf("Is %d > %d?\n", profileOfRankings[c1+(r*nc)], profileOfRankings[c2+(r*nc)]);
       }
-      
-      // printf("The position of c1 > c2 is: %d \n", c2+(c1*nc));
-      // printf("The position of c2 > c1 is: %d \n", c1+(c2*nc));
       // Update the row
       votrix[c2+(c1*nc)] = total;
       // Update the column
