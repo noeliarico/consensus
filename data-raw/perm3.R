6a8d7db0a33f5a25e29c632dbd80e3a7eebba475
noeliarico <noeliarico@uniovi.es>
2020-09-08 16:40:25 +0200

    Add perms3 data

--- data-raw/perm3.R
+++ data-raw/perm3.R
@@ -1,3 +1,7 @@
 ## code to prepare `perm3` dataset goes here
-
+num <- 3
+file_name <- paste0("perm", num, ".csv")
+perm <- read.csv(file.path("perms", file_name), header = FALSE)
+colnames(perm) <- letters[1:num]
+perm3 <- profile_of_rankings(perm)
 usethis::use_data(perm3, overwrite = TRUE)
