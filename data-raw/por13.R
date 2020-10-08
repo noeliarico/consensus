8c1b417047f4639a76dfb1dc63ad5a6e2dd1ba6e
Noelia <noeliarico@uniovi.es>
2020-10-08 12:16:25 +0200

    Add por13

--- data-raw/por13.R
+++ data-raw/por13.R
@@ -1,7 +1,7 @@
 ## code to prepare `por13` dataset goes here
 
 por13 <- parse_profile_of_rankings("5, a ≻ b ≻ c ≻ d,
-                                    3, b ≻ c ≻ d ≻ a,
+                                    4, b ≻ c ≻ d ≻ a,
                                     2, c ≻ d ≻ b ≻ a,
                                     1, d ≻ c ≻ b ≻ a")
 
