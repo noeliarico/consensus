db0461d9382d8496580a2448823a609d563aaf88
Noelia <noeliarico@uniovi.es>
2020-11-12 09:23:20 +0100

    Update por08

--- data-raw/por08.R
+++ data-raw/por08.R
@@ -1,9 +1,13 @@
 ## code to prepare `por08` dataset goes here
 
-por08 <- structure(list(profileOfRankings = structure(list(a = c(4L, 
-                                                                  1L, 2L, 3L, 2L, 4L, 1L, 3L, 3L), b = c(3L, 3L, 1L, 2L, 3L, 2L, 
-                                                                                                          3L, 2L, 4L), c = c(2L, 2L, 3L, 1L, 1L, 1L, 4L, 4L, 1L), d = c(1L, 
-                                                                                                                                                                          4L, 4L, 4L, 4L, 3L, 2L, 1L, 2L)), row.names = c(NA, -9L), class = "data.frame"), 
-                        numberOfVoters = c(1, 1, 1, 1, 1, 1, 1, 2, 1), candidates = c("a", 
-                                                                                      "b", "c", "d")), class = c("por", "list"))
+por08 <- parse_profile_of_rankings(c("    
+    1, C1 > C3 > C2 > C4, 
+    1, C4 > C3 > C1 > C2, 
+    2, C3 > C2 > C4 > C1, 
+    1, C2 > C1 > C4 > C3, 
+    2, C1 > C4 > C2 > C3, 
+    1, C2 > C4 > C1 > C3, 
+    1, C2 > C4 > C3 > C1, 
+    1, C4 > C1 > C3 > C2"))
+  
 usethis::use_data(por08, overwrite = TRUE)
