3ae2c64a493c9029ec35b7bf3d1d1f94796e6b1d
Noelia <noeliarico@uniovi.es>
2021-01-18 08:55:23 +0100

    Add por16

---
+++ data-raw/por16-R.R
@@ -0,0 +1,6 @@
+por16 <- structure(list(profileOfRankings = structure(list(C1 = c(4L, 
+                                                                  3L, 3L, 2L), C2 = c(1L, 2L, 4L, 3L), C3 = c(2L, 1L, 1L, 4L), 
+                                                           C4 = c(3L, 4L, 2L, 1L)), row.names = c(NA, -4L), class = "data.frame"), 
+                        numberOfVoters = c(4, 2, 4, 5), candidates = c("C1", "C2", 
+                                                                       "C3", "C4")), class = c("por", "list"))
+usethis::use_data(por16, overwrite = TRUE)
